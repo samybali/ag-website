c3abb6eda7fb6adcebe0800d51ad32bd5422ab01
Samy Bali <samy.bali@telecom-paris.fr>
2026-01-18 00:47:33 +0100

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-
-  // Configuration du serveur
-  server: {
-    host: true,
-    port: 3000
-  },
-
-  // Configuration de Vite (C'est ici qu'on corrige l'erreur)
-  vite: {
-    preview: {
-      host: true,
-      port: 3000,
-      // On liste TOUS les noms possibles explicitement
-      allowedHosts: [
-        '19wires.com',
-        'www.19wires.com',
-        'localhost',
-        '127.0.0.1'
-      ]
-    }
-  }
+  // On laisse Astro compiler le site normalement.
+  // Plus besoin de config "server" ou "vite" ici car on utilise "serve" en prod.
 });

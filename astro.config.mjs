04d34822f21596800694b5898fd69682d2d5f9d4
Samy Bali <samy.bali@telecom-paris.fr>
2026-01-17 23:46:56 +0100

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  server: { host: true, port: 3000 }
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });

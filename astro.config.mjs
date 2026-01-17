21dc9251268ba120c6c078ce09142f9d8f0ba8f4
Samy Bali <samy.bali@telecom-paris.fr>
2026-01-17 23:36:52 +0100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    server: {
-      host: true, // Écoute sur toutes les adresses (0.0.0.0)
-    },
-    preview: {
-      host: true,        // Force l'écoute sur le réseau
-      allowedHosts: true, // ✅ DÉSACTIVE LA SÉCURITÉ DES NOMS DE DOMAINE
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    },
-  },
+  server: { host: true, port: 3000 }
 });
